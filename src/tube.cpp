fa794a0fce652c5f5d3c3995a512752c257c8b95
HugoLaullier <hugo.laullier@gmail.com>
2022-04-08 12:25:21 +0200

    level resest

--- src/tube.cpp
+++ src/tube.cpp
@@ -4,7 +4,7 @@ Tube::Tube(SDL_Renderer *renderer, int level) : renderer(renderer),
                                                 level(level)
 {
     // SQUARE
-    if (level == 100)
+    if (level == 1)
     {
         vertices.push_back(std::make_pair(200, 200));
         vertices.push_back(std::make_pair(600, 200));
@@ -39,7 +39,7 @@ Tube::Tube(SDL_Renderer *renderer, int level) : renderer(renderer),
     }
 
     // INFINITY
-    if (level == 1)
+    if (level == 4)
     {
         vertices.push_back(std::make_pair(100, 400));
         vertices.push_back(std::make_pair(200, 300));
